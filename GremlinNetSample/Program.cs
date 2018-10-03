a3c94a1689ca499c7b99c3c2df251a91be621a02
Oliver Towers <olivert@microsoft.com>
2018-10-03 12:38:26 -0700

    Add use of ResultSet and StatusAttributes to the sample, with descriptions of available attributes. Requires bumping Gremlin.NET to 3.4.0-rc2

--- GremlinNetSample/Program.cs
+++ GremlinNetSample/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Gremlin.Net;
 using Gremlin.Net.Driver;
-using Newtonsoft.Json;
+using Gremlin.Net.Driver.Exceptions;
 using Gremlin.Net.Structure.IO.GraphSON;
+using Newtonsoft.Json;
 
 namespace GremlinNetSample
 {
@@ -61,15 +59,26 @@ namespace GremlinNetSample
                     Console.WriteLine(String.Format("Running this query: {0}: {1}", query.Key, query.Value));
 
                     // Create async task to execute the Gremlin query.
-                    var task = gremlinClient.SubmitAsync<dynamic>(query.Value);
-                    task.Wait();
-
-                    foreach (var result in task.Result)
+                    var resultSet = SubmitRequest(gremlinClient, query).Result;
+                    if (resultSet.Count > 0)
                     {
-                        // The vertex results are formed as Dictionaries with a nested dictionary for their properties
-                        string output = JsonConvert.SerializeObject(result);
-                        Console.WriteLine(String.Format("\tResult:\n\t{0}", output));
+                        Console.WriteLine("\tResult:");
+                        foreach (var result in resultSet)
+                        {
+                            // The vertex results are formed as Dictionaries with a nested dictionary for their properties
+                            string output = JsonConvert.SerializeObject(result);
+                            Console.WriteLine($"\t{output}");
+                        }
+                        Console.WriteLine();
                     }
+
+                    // Print the status attributes for the result set.
+                    // This includes the following:
+                    //  x-ms-status-code            : This is the sub-status code which is specific to Cosmos DB.
+                    //  x-ms-total-request-charge   : The total request units charged for processing a request.
+                    Console.WriteLine($"\tStatus Attributes:");
+                    Console.WriteLine($"\t[\"x-ms-status-code\"] : { JsonConvert.SerializeObject(resultSet.StatusAttributes["x-ms-status-code"])}");
+                    Console.WriteLine($"\t[\"x-ms-total-request-charge\"] : { JsonConvert.SerializeObject(resultSet.StatusAttributes["x-ms-total-request-charge"])}");
                     Console.WriteLine();
                 }
             }
@@ -77,6 +86,35 @@ namespace GremlinNetSample
             // Exit program
             Console.WriteLine("Done. Press any key to exit...");
             Console.ReadLine();
+        }
+
+        private static Task<ResultSet<dynamic>> SubmitRequest(GremlinClient gremlinClient, KeyValuePair<string, string> query)
+        {
+            try
+            {
+                return gremlinClient.SubmitAsync<dynamic>(query.Value);
+            }
+            catch (ResponseException e)
+            {
+                Console.WriteLine("\tRequest Error!");
+
+                // Print the Gremlin status code.
+                Console.WriteLine($"\tStatusCode: {e.StatusCode}");
+
+                // On error, ResponseException.StatusAttributes will include the common StatusAttributes for successful requests, as well as
+                // additional attributes for retry handling and diagnostics.
+                // These include:
+                //  x-ms-retry-after-ms         : The number of milliseconds to wait to retry the operation after an initial operation was throttled. This will be populated when
+                //                              : attribute 'x-ms-status-code' returns 429.
+                //  x-ms-activity-id            : Represents a unique identifier for the operation. Commonly used for troubleshooting purposes.
+                Console.WriteLine($"\tStatusAttributes:");
+                Console.WriteLine($"\t[\"x-ms-status-code\"] : { JsonConvert.SerializeObject(e.StatusAttributes["x-ms-status-code"])}");
+                Console.WriteLine($"\t[\"x-ms-total-request-charge\"] : { JsonConvert.SerializeObject(e.StatusAttributes["x-ms-total-request-charge"])}");
+                Console.WriteLine($"\t[\"x-ms-retry-after-ms\"] : { JsonConvert.SerializeObject(e.StatusAttributes["x-ms-retry-after-ms"])}");
+                Console.WriteLine($"\t[\"x-ms-activity-id\"] : { JsonConvert.SerializeObject(e.StatusAttributes["x-ms-activity-id"])}");
+
+                throw;
+            }
         }
     }
 }
